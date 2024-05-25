4c65e2ccf7c6a87d36e60c6da4c2658625004b59
An Pham <quocan01255@gmail.com>
2024-05-25 19:48:00 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use('/static', express.static('public'));
 
